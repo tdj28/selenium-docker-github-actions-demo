f93661a1213b8a0ee598a555c3d15a59f6379e08
tdj28 <ti.do.jo@gmail.com>
2022-07-24 21:35:17 -0700

    fix: add & after nohup call to bg

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #! /bin/bash -e
 
 #Xvfb :99 -screen 0 640x480x8 -nolisten tcp &
-python3 flask-helloworld.py &
+#python3 flask-helloworld.py &
+
+nohup python3 flask-helloworld.py &
 
 #xvfb-run --listen-tcp --server-num 44 --auth-file /tmp/xvfb.auth -s "-ac -screen 0 640x480x8"
 
